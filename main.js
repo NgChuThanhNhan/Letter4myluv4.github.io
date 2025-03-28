c2b6c7325730e9c38a236a5bac3ceefac1be2327
Nguyễn Chu Thành Nhân <5001104105@student.hcmue.edu.vn>
2025-03-28 08:43:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,23 @@ function effectWrite() {
 }
 
 // Nhạc nền
-var audio = new Audio("music.mp3"); // Thay bằng đường dẫn đúng của file nhạc
+var audio = new Audio("music.mp3"); // Đảm bảo đường dẫn đúng
 audio.loop = true; // Phát lặp lại
+
+// Chỉ phát nhạc khi người dùng click
+function playMusic() {
+    audio.play().then(() => {
+        console.log("Nhạc đang phát...");
+    }).catch(error => {
+        console.log("Trình duyệt chặn autoplay:", error);
+    });
+
+    // Sau khi phát nhạc, xóa sự kiện click để tránh lặp lại
+    document.removeEventListener("click", playMusic);
+}
+
+// Khi trang load, chờ người dùng click mới phát nhạc
+document.addEventListener("click", playMusic);
 
 window.addEventListener("load", () => {
     setTimeout(() => {
@@ -39,12 +54,9 @@ cardValentine.addEventListener("click", () => {
 
     if (cardValentine.classList.contains("open")) {
         setTimeout(effectWrite, 500);
-        audio.play(); // Phát nhạc khi mở thư
     } else {
         setTimeout(() => {
             document.querySelector(".letterContent").innerHTML = "";
         }, 1000);
-        audio.pause(); // Dừng nhạc khi đóng thư
-        audio.currentTime = 0; // Reset nhạc về đầu
     }
 });
